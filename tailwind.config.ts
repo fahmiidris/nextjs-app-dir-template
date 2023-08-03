663f012ec1c86e4ea91bf6f5b6a00e2bacffb9b2
Fahmi Idris <fahmiidris.1607@gmail.com>
2023-08-03 12:37:03 +0700

    feat: move font feature settings to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const tailwindConfig = {
                 primary: colors.cyan,
             },
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: [['Inter var', ...defaultTheme.fontFamily.sans], { fontFeatureSettings: "'cv02', 'cv03', 'cv04', 'cv11'" }],
             },
             screens: {
                 '2xl': '1536px',
