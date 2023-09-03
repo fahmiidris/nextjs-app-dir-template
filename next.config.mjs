c935691cf670a186ec6fc565e1743d9a88337102
Fahmi Idris <fahmiidris.dev@gmail.com>
2023-09-03 17:04:34 +0700

    feat: add next bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
