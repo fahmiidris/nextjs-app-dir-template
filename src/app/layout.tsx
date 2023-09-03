13bf916efdb1c9f00079d94f25916c7646cc1adc
Fahmi Idris <fahmiidris.dev@gmail.com>
2023-09-03 13:07:46 +0700

    feat: add metadata and rename folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,20 @@ import * as React from 'react';
 
 import clsxm from '@/utils/clsxm';
 
+import type { Metadata } from 'next';
 import type { TLayoutProps } from '@/types/layout.type';
 
-export default function RootLayout({ children }: TLayoutProps) {
+const DEFAULT_TITLE = 'Next.js App Dir Template';
+
+export const metadata: Metadata = {
+    title: {
+        template: `%s - ${DEFAULT_TITLE}`,
+        default: DEFAULT_TITLE,
+    },
+    description: 'Next.js app directory template, built with Next.js (TypeScript) and Tailwind CSS.',
+};
+
+export default function RootLayout({ children }: TLayoutProps): JSX.Element {
     return (
         <html lang="en" className={clsxm('scroll-smooth [--scroll-mt:9.875rem]', 'lg:[--scroll-mt:6.3125rem]')}>
             <body className="bg-white font-sans text-slate-500 antialiased">{children}</body>
