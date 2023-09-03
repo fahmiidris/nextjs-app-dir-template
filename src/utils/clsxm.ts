6cc7aa75aafbca92b691c58b634fa8f7f595f77c
Fahmi Idris <fahmiidris.dev@gmail.com>
2023-09-03 13:08:23 +0700

    feat: add return type

--- src/utils/clsxm.ts
+++ src/utils/clsxm.ts
@@ -1,6 +1,6 @@
 import clsx, { type ClassArray } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export default function clsxm(...classNames: ClassArray) {
+export default function clsxm(...classNames: ClassArray): string {
     return twMerge(clsx(...classNames));
 }
