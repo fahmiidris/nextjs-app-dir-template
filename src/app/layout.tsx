7ebe2db600ea858b2312d3efff1085785769a044
Fahmi Idris <fahmiidris.dev@gmail.com>
2023-09-17 02:55:02 +0700

    style: rename function and add missing type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,17 +8,18 @@ import clsxm from '@/utils/clsxm';
 import type { Metadata } from 'next';
 import type { TLayoutProps } from '@/types/layout.type';
 
-const DEFAULT_TITLE = 'Next.js App Dir Template';
+const DEFAULT_TITLE: string = 'Next.js App Dir Template';
+const DEFAULT_DESCRIPTION: string = 'Next.js app directory template, built with Next.js (TypeScript) and Tailwind CSS.';
 
 export const metadata: Metadata = {
     title: {
         template: `%s - ${DEFAULT_TITLE}`,
         default: DEFAULT_TITLE,
     },
-    description: 'Next.js app directory template, built with Next.js (TypeScript) and Tailwind CSS.',
+    description: DEFAULT_DESCRIPTION,
 };
 
-export default function RootLayout({ children }: TLayoutProps): JSX.Element {
+export default function Layout({ children }: TLayoutProps): JSX.Element {
     return (
         <html lang="en" className={clsxm('scroll-smooth [--scroll-mt:9.875rem]', 'lg:[--scroll-mt:6.3125rem]')}>
             <body className="bg-white font-sans text-slate-500 antialiased">{children}</body>
