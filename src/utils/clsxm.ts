ed4bcae75514ef6fe0d2d48699797d055838741d
Fahmi Idris <fahmiidris.dev@gmail.com>
2023-09-17 17:03:45 +0700

    style: remove destructuring

--- src/utils/clsxm.ts
+++ src/utils/clsxm.ts
@@ -2,5 +2,5 @@ import clsx, { type ClassArray } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export default function clsxm(...classNames: ClassArray): string {
-    return twMerge(clsx(...classNames));
+    return twMerge(clsx(classNames));
 }
