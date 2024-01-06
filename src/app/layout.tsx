0e62f7d3317a1f4c21277673248d1ed2aaac8e69
Fahmi Idris <fahmiidris.dev@gmail.com>
2024-01-06 22:30:08 +0700

    feat: rename clsxm utility (#6)
    * feat: rename clsxm utils
    
    * feat: add new type for page props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@/styles/main.css';
 
 import * as React from 'react';
 
-import clsxm from '@/utils/clsxm';
+import cn from '@/utils/cn';
 
 import type { Metadata } from 'next';
 import type { TLayoutProps } from '@/types/layout.type';
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: TLayoutProps) {
     return (
-        <html lang="en" className={clsxm('scroll-smooth [--scroll-mt:9.875rem]', 'lg:[--scroll-mt:6.3125rem]')}>
+        <html lang="en" className={cn(['scroll-smooth [--scroll-mt:9.875rem]', 'lg:[--scroll-mt:6.3125rem]'])}>
             <body className="bg-white font-sans text-slate-500 antialiased">{children}</body>
         </html>
     );
