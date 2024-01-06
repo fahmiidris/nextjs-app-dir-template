63649f88bc301ba1b8e6c058e89f0fe85efc5ffe
Fahmi Idris <fahmiidris.dev@gmail.com>
2024-01-06 22:26:52 +0700

    feat: rename clsxm utils

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,7 +1,6 @@
 import { twMerge } from 'tailwind-merge';
-
 import clsx, { type ClassArray } from 'clsx';
 
-export default function clsxm(...classNames: ClassArray) {
+export default function cn(...classNames: ClassArray) {
     return twMerge(clsx(classNames));
 }
