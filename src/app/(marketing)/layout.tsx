6a205642eb362a82a1069eb14e8e14ec7001267b
Fahmi Idris <fahmiidris.dev@gmail.com>
2024-01-23 19:56:31 +0700

    feat: remove marketing folder

--- src/app/(marketing)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import type { TLayoutProps } from '@/types/layout.type';
-
-export default async function Layout({ children }: TLayoutProps) {
-    return <main className="relative">{children}</main>;
-}
