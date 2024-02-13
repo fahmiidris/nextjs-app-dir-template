aac36f78763ddc76b07e9cfaa547cdfdf9c5ceb5
Fahmi Idris <fahmiidris.dev@gmail.com>
2024-02-13 20:34:55 +0700

    fix: error build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,16 @@ import type { Metadata } from 'next';
 
 import type { TLayoutProps } from '@/types/layout';
 
+export default async function Layout({ children }: TLayoutProps) {
+    return (
+        <html lang="en" className={cn(['scroll-smooth [--scroll-mt:9.875rem]', 'lg:[--scroll-mt:6.3125rem]'])} suppressHydrationWarning>
+            <body className={cn(['bg-white font-sans text-slate-500 antialiased', 'dark:bg-zinc-700 dark:text-white'])}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
+}
+
 const DEFAULT_TITLE = 'Next.js App Dir Template';
 const DEFAULT_DESCRIPTION = 'Next.js app directory template, built with Next.js (TypeScript) and Tailwind CSS.';
 
@@ -21,13 +31,3 @@ export const metadata: Metadata = {
     },
     description: DEFAULT_DESCRIPTION,
 };
-
-export default async function Layout({ children }: TLayoutProps) {
-    return (
-        <html lang="en" className={cn(['scroll-smooth [--scroll-mt:9.875rem]', 'lg:[--scroll-mt:6.3125rem]'])} suppressHydrationWarning>
-            <body className={cn(['bg-white font-sans text-slate-500 antialiased', 'dark:bg-zinc-700 dark:text-white'])}>
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    );
-}
--- src/types/layout.ts
+++ src/types/layout.ts
@@ -1,8 +1,10 @@
 import type { PropsWithChildren } from 'react';
 
-import type { TParams, TSearchParams } from '@/types/params';
+// import type { TParams, TSearchParams } from '@/types/params';
 
-export type TLayoutProps<P = TParams, SP = TSearchParams> = PropsWithChildren<{
-    params: P;
-    searchParams: SP;
-}>;
+// export type TLayoutProps<P = TParams, SP = TSearchParams> = PropsWithChildren<{
+//     params: P;
+//     searchParams: SP;
+// }>;
+
+export type TLayoutProps<T = unknown> = PropsWithChildren<T>;
